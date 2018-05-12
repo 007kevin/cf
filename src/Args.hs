194917fce34495ccd1d98c46868848ae2ca6a7ee
local <local>
2018-05-12 02:02:56 +0000

    Got login command to work

---
+++ src/Args.hs
@@ -0,0 +1,42 @@
+module Args (start) where
+
+import Options.Applicative
+import Data.Monoid
+
+data Args = Args Command
+  deriving Show
+
+data Command
+  = Login LoginOpts
+  deriving Show
+
+data LoginOpts = LoginOpts
+  { username :: String }
+  deriving Show
+
+version :: Parser (a -> a)
+version = infoOption "0.0.0"
+  ( long "version" <>
+    help "Print version information" )
+
+loginParser :: Parser Command
+loginParser = Login <$> loginOpts
+  
+loginOpts :: Parser LoginOpts
+loginOpts = LoginOpts <$> argument str (metavar "HANDLE")
+
+parser :: Parser Args
+parser = Args <$>
+  subparser ( command "login"
+              ( info loginParser
+                ( progDesc "Log into Codeforces" )))
+
+opts = info ( helper <*> version <*> parser ) idm
+
+run :: Args -> IO()
+run (Args (Login handle)) = putStrLn ("logging in as " ++ (username handle))
+
+start :: IO ()
+start = execParser opts >>= run
+  
+  
