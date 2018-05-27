09d134a2e7d2a345e76c8d2931519b8631b39772
local <local>
2018-05-27 13:22:32 +0000

    update

--- src/Login.hs
+++ src/Login.hs
@@ -12,27 +12,34 @@ import qualified Network.Wreq.Session as S
 initLogin :: String -> IO()
 initLogin handle =  putStrLn handle
 
--- TODO: Refactor ugly code with maybe monad error handling
-extractCsrf :: Response C.ByteString -> String
-extractCsrf = head.tail.head.regex.C.unpack.(^. responseBody)
+extractCsrf :: Response C.ByteString -> Maybe String
+extractCsrf res =
+  do match <- regex (C.unpack (res ^. responseBody))
+     return $ last (head match) 
   where
-    regex :: String -> [[String]]
-    regex = (=~ ("name='csrf_token' +value='([^\']+)'"::String))
+    regex :: String -> Maybe [[String]]
+    regex = (=~~ ("name='csrf_token' +value='([^\']+)'"::String))
 
-login :: IO ()
-login = let loginURL = M.url ++ "/enter" in
-  do session <- S.newSession
-     csrf_token <- extractCsrf <$> S.get session loginURL
-     res <- S.post session loginURL [ "action"        := ("enter"::String),
-                                      "handleOrEmail" := (""::String),
-                                      "password"      := (""::String),
-                                      "remember"      := ("yes"::String),
-                                      "csrf_token"         := (csrf_token::String) ]
-     print $ testLogin (C.unpack (res ^. responseBody))
-     cookie <- S.getSessionCookieJar session
-     case cookie of
-       Just jar -> pPrint jar
-       Nothing -> print "Cookie not found"
+login1 :: IO ()
+login1 = let loginURL = M.url ++ "/enter" in
+  do maybeToken <- fmap extractCsrf (get loginURL)
+     return $ do token <- maybeToken
+                 return token
+
+-- login :: IO ()
+-- login = let loginURL = M.url ++ "/enter" in
+--   do session <- S.newSession
+--      csrf_token <- extractCsrf <$> S.get session loginURL
+--      res <- S.post session loginURL [ "action"        := ("enter"::String),
+--                                       "handleOrEmail" := (""::String),
+--                                       "password"      := (""::String),
+--                                       "remember"      := ("yes"::String),
+--                                       "csrf_token"         := (csrf_token::String) ]
+--      print $ testLogin (C.unpack (res ^. responseBody))
+--      cookie <- S.getSessionCookieJar session
+--      case cookie of
+--        Just jar -> pPrint jar
+--        Nothing -> print "Cookie not found"
 
 testLogin :: String -> String
 testLogin = (=~ ("\"error for__password\""::String))
